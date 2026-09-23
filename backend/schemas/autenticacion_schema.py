3ca02cb50b5e5dbb0f0267b6b6fbe4dafdf78e6f
Robert7-r <cbi2173050310@izt.uam.mx>
2026-09-23 14:10:01 -0600

    Backend para la funcionalidad de login y ajustes en frontend

--- backend/schemas/autenticacion_schema.py
+++ backend/schemas/autenticacion_schema.py
@@ -1,9 +1,14 @@
-# Schemas/autenticacion_schema.py
+# schemas/autenticacion_schema.py
 
-# BaseModel -- clase para crear dtos, EmailStr -- valida que el texto tenga formato de correo
+# BaseModel -- clase para crear dtos, EmailStr -- valida que el texto tenga formato de corre
+# EmailStr: verifica si un texto tiene forma 
+# de correo electrónico (ej. que tenga un "@" y un ".com").
 from pydantic import BaseModel, EmailStr
 
 # DTO de entrada: datos que el cliente debe mandar para iniciar sesion
+# DTO de entrada (Data Transfer Object): 
 class LoginRequest(BaseModel):
+    # Si el Frontend manda "juan" en vez de "juan@mail.com", Pydantic lo bloquea.
     correo: EmailStr
+    # Obliga a que la contraseña venga y sea texto.
     contrasena: str
